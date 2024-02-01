678257f259329a9a63413ed2767a3fefd45c6f5a
Louarn RICHARD <contact@louarn.xyz>
2024-02-01 10:41:05 +0100

    Player movement

--- scenes/character.gd
+++ scenes/character.gd
@@ -1,28 +1,39 @@
+class_name Character
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# The player's movement speed, set by default to 6 tiles per second
+@export var speed: float = 6.0 * 32.0
+# The player's jump force, set by default to 16 tiles per second
+@export var jump_speed: float = -16.0 * 32.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+# The collected coins counter
+var _coins: int = 0
+# And the label which will display the collected coins
+@onready var _coins_display = $Label
+
+
+func add_coins(amount: int) -> void:
+	_coins += amount
+	_coins_display.text = "Coins: " + str(_coins)
+
+
 func _physics_process(delta):
-	# Add the gravity.
+	var direction = Input.get_axis("MoveLeft", "MoveRight")
+	velocity.x = direction * speed
+	
+	if Input.is_action_pressed("Jump") and is_on_floor():
+		velocity.y += jump_speed
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	
+	
+func _ready():
+	_coins_display.text = "Coins: " + str(_coins)
---
+++ scenes/coin.gd
@@ -0,0 +1,19 @@
+class_name Coin
+extends Area2D
+
+
+@export var value: int = 1
+
+
+# Function called when the coin is created
+func _ready():
+	# We tell the coin to call the function _on_body_entered when an object collides with it
+	body_entered.connect(_on_body_entered)
+
+
+# Function meant to be call when there's a collision between a body and the coin
+func _on_body_entered(body: Node2D) -> void:
+	# We start by checking if the body is a character
+	if body is Character:
+		# If it is, we add coins to the character
+		body.add_coins(value)
